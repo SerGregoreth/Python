018f99ea65fd63bbac4ce0a83b66edcfb51adf08
Gregoreth <ghernandez0130@gmail.com>
2020-07-23 13:05:09 -0400

    initial commit with dest_i func

---
+++ tourist.py
@@ -0,0 +1,11 @@
+destinations = ['Paris, France', 'Shanghai, China', 'Los Angeles, USA', 'Sao Paolo, Brazil', 'Cairo, Egypt']
+test_subj = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
+#getting the index of each element in destination list
+def get_destination_i(destination):
+    destination_i = 0
+    for place in destinations:
+        if place == destination:
+            destination_i = destinations.index(place)
+            return destination_i 
+
