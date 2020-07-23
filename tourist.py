67fc295b59add9368eb3522b8dc408d5a481fed8
Gregoreth <ghernandez0130@gmail.com>
2020-07-23 14:01:10 -0400

    confirm functionality of traveler_loc func

--- tourist.py
+++ tourist.py
@@ -8,18 +8,14 @@ def get_destination_i(destination):
         if place != destination:
             continue
         elif place == destination:
-            destination_i = destinations.index(destination)
+            destination_i = destinations.index(place)
             return destination_i
         
 #Determining traveler location:
 def traveler_loc(traveler):
-    traveler_dest = traveler.index(1)
-    for place in destinations:
-        if traveler_dest != place:
-            continue
-        elif traveler_dest == place:
-            traveler_dest_i = destinations.index(place)
-            return traveler_dest_i
+    traveler_dest = traveler[1]
+    traveler_dest_i = get_destination_i(traveler_dest)
+    return traveler_dest_i
 
 test_dest_i = traveler_loc(traveler_tst)
 print(test_dest_i)
